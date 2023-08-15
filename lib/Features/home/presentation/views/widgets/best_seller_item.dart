2547e5429bf889ef466314ab48125a142d8a6103
khaledgamal22 <kghaly56@gmail.com>
2023-08-15 16:55:16 +0300

    create book details file

--- lib/Features/home/presentation/views/widgets/best_seller_item.dart
+++ lib/Features/home/presentation/views/widgets/best_seller_item.dart
@@ -1,8 +1,6 @@
 import 'package:book_app/constant.dart';
 import 'package:book_app/core/utilits/styles.dart';
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-
 import 'book_rating.dart';
 
 class BestSellerItem extends StatelessWidget {
