fa8e037da50004bd022041a8e7fe81e26b94589c
khaledgamal22 <kghaly56@gmail.com>
2023-08-17 18:29:23 +0300

    create and done newest cubit

--- lib/Features/home/presentation/view_models/featured_books_cubit/featured_books_cubit.dart
+++ lib/Features/home/presentation/view_models/featured_books_cubit/featured_books_cubit.dart
@@ -1,8 +1,6 @@
 import 'package:bloc/bloc.dart';
 import 'package:book_app/Features/home/data/models/book_model/book_model.dart';
 import 'package:book_app/Features/home/data/repos/home_repo.dart';
-import 'package:book_app/Features/home/data/repos/home_repo_implement.dart';
-import 'package:book_app/core/utilits/api_services.dart';
 import 'package:equatable/equatable.dart';
 
 part 'featured_books_state.dart';
---
+++ lib/Features/home/presentation/view_models/newest_books_cubit/newest_books_cubit.dart
@@ -0,0 +1,25 @@
+
+import 'package:bloc/bloc.dart';
+import 'package:book_app/Features/home/data/models/book_model/book_model.dart';
+import 'package:book_app/Features/home/data/repos/home_repo.dart';
+import 'package:equatable/equatable.dart';
+
+part 'newest_books_state.dart';
+
+class NewestBooksCubit extends Cubit<NewestBooksState> {
+  NewestBooksCubit(this.homeRepo) : super(NewestBooksInitial());
+  final HomeRepo homeRepo;
+
+  Future<void> get() async {
+    emit(NewestBooksLoading());
+    var data = await homeRepo.fetechNewestBooks();
+    data.fold(
+      (failure) {
+        emit(NewestBooksFailure(failure.errorMessage));
+      },
+      (books) {
+        emit(NewestBooksSuccess(books));
+      },
+      );
+  }
+}
