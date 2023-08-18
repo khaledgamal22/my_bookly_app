e16aa049f55a5cbcd55788306a698752fee8a29c
khaledgamal22 <kghaly56@gmail.com>
2023-08-18 14:12:35 +0300

    create service locator use get it

---
+++ lib/core/utilits/service_locator.dart
@@ -0,0 +1,19 @@
+import 'package:book_app/Features/home/data/repos/home_repo_implement.dart';
+import 'package:book_app/core/utilits/api_services.dart';
+import 'package:dio/dio.dart';
+import 'package:get_it/get_it.dart';
+
+final getIt = GetIt.instance;
+
+void setup() {
+  getIt.registerSingleton<ApiService>(
+    ApiService(
+      Dio(),
+    ),
+  );
+  getIt.registerSingleton<HomeRepoImplementation>(
+    HomeRepoImplementation(
+      getIt.get<ApiService>(),
+    ),
+  );
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:book_app/constant.dart';
 import 'package:book_app/core/utilits/app_routes.dart';
+import 'package:book_app/core/utilits/service_locator.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 void main() {
+  setup();
   runApp(const BooklyApp());
 }
 
